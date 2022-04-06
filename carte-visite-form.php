62a2e35dc5099bcad8994665c62d7955644a8c66
zimjunior <kndiaye@baobabgroup.com>
2022-04-06 15:57:18 +0000

    Add config

--- carte-visite-form.php
+++ carte-visite-form.php
@@ -26,3 +26,33 @@ if( class_exists( 'CarteVisiteForm')) {
 }
 add_action( 'wp_body_open', $item->show_link() );
 
+function initialize(){
+	global $wpdb;
+	$table_name = $wpdb->prefix . "demande_plaques";
+	$wp_demande_plaques_db_version = '1.0.0';
+	$charset_collate = $wpdb->get_charset_collate();
+	if ( $wpdb->get_var("SHOW TABLES LIKE '{$table_name}'") != $table_name ) {	
+		$sql = "CREATE TABLE $table_name (
+				ID mediumint(9) NOT NULL AUTO_INCREMENT,
+				`demande-plaque-number` text NOT NULL,
+				`demande-plaque-qte` int(9) NOT NULL,
+				`demande-plaque-kitpose` boolean DEFAULT false,
+				`demande-plaque-region` varchar(50) NOT NULL,
+				`demande-plaque-num-dep` int(9) NOT NULL,
+				`demande-plaque-text` varchar(50)  NOT NULL,
+				`demande-plaque-materiaux` varchar(50) NOT NULL,
+				`demande-plaque-status` int(9) NOT NULL,
+				`demande-plaque-name-customer` varchar(50) NOT NULL,
+				`demande-plaque-email` varchar(50) NOT NULL,
+				PRIMARY KEY  (ID)
+		) $charset_collate;";
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql);
+		// add_option('my_db_version', $wp_demande_plaques_db_version);
+		
+	}
+	
+}
+register_activation_hook( __FILE__, 'initialize' );
+
+
